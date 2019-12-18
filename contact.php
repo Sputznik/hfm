e001c46dfa22fbbe6a67ac49c299bde7a2334349
unknown <samvthom16@gmail.com>
2019-12-18 20:26:18 +0530

    Update contact styles

--- contact.php
+++ contact.php
@@ -35,12 +35,12 @@
             <div class="col-md-6 col-sm-6 col-xs-12">
               <?php foreach( $data as $key => $value):?>
               <div class="contact-info">
-                <div class="contact-icon pull-left" style="margin-right: 20px;">
+                <div class="contact-icon">
                   <i class="<?php echo $value['icon']?>"></i>
                 </div>
-                <div class="push-right">
+                <div>
                   <h2><?php echo $value['h2']?></h2>
-                  <h5><?php echo $value['h4']?></h5>
+                  <p><?php echo $value['h4']?></p>
                 </div>
               </div>
               <?php endforeach;?>
@@ -80,11 +80,19 @@
 .contact-widget .contact-info{
   color: #2E4857;
   margin-bottom: 40px;
+  display: grid;
+  grid-template-columns: 60px 1fr;
+  grid-gap: 30px;
 }
 
-.contact-widget .contact-info h5{
+.contact-widget .contact-info h2{
+  margin-top: 0;
+  font-size: 35px;
+}
+
+.contact-widget .contact-info p{
   color: #707070;
-  font-family: 'Josefin Sans';
+  font-size: 20px;
 }
 
 
@@ -98,7 +106,8 @@
 }
 .contact-icon{display: inline-block;}
 .contact-icon i{
-  font-size: 30px;
+  font-size: 60px;
+
 }
 @media(max-width: 768px){
   .contact-details{
