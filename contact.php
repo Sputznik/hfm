aeaab14946305f463f3a06b0a34d2aae5868c6da
unknown <samvthom16@gmail.com>
2019-12-19 17:33:24 +0530

    Update contact css for mobile

--- contact.php
+++ contact.php
@@ -24,43 +24,45 @@
   );
 ?>
 <div class="container-fluid contact-widget">
-  <div class="row">
-      <div class="bg-contact" style="background-image: url(<?php _e( $contact_image );?>);">
-        <div class="overlay"></div>
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-6 col-md-6 pull-left col-xs-12">
-              <h1>Get in Touch</h1>
-            </div>
-            <div class="col-md-6 col-sm-6 col-xs-12">
-              <?php foreach( $data as $key => $value):?>
-              <div class="contact-info">
-                <div class="contact-icon">
-                  <i class="<?php echo $value['icon']?>"></i>
-                </div>
-                <div>
-                  <h2><?php echo $value['h2']?></h2>
-                  <p><?php echo $value['h4']?></p>
-                </div>
-              </div>
-              <?php endforeach;?>
-            </div>
+  <div class="bg-contact" style="background-image: url(<?php _e( $contact_image );?>);"></div>
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6 col-md-6 pull-left col-xs-12">
+        <h1>Get in Touch</h1>
+      </div>
+      <div class="col-md-6 col-sm-6 col-xs-12">
+        <?php foreach( $data as $key => $value):?>
+        <div class="contact-info">
+          <div class="contact-icon">
+            <i class="<?php echo $value['icon']?>"></i>
+          </div>
+          <div>
+            <h2><?php echo $value['h2']?></h2>
+            <p><?php echo $value['h4']?></p>
           </div>
         </div>
+        <?php endforeach;?>
       </div>
     </div>
   </div>
 </div>
 
 <style>
+.contact-widget{
+  position: relative;
+  height: 100vh;
+}
 .contact-widget .bg-contact{
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   color: #fff;
-  position: relative;
+  top: 0;
+  left: 0;
+  position: absolute;
   display: block;
 }
 .contact-widget .overlay{
@@ -74,11 +76,10 @@
 }
 .contact-widget h1{
   font-size: 64px;
+  color: #fff;
 }
 
-.contact-widget .container{
-  padding-top: 150px;
-}
+.contact-widget .container{ padding-top: 150px; }
 
 .contact-widget .contact-info{
   color: #2E4857;
@@ -88,10 +89,7 @@
   grid-gap: 30px;
 }
 
-.contact-widget .contact-info h2{
-  margin-top: 0;
-  font-size: 35px;
-}
+.contact-widget .contact-info h2{ margin-top: 0; font-size: 35px; }
 
 .contact-widget .contact-info p{
   color: #707070;
@@ -101,13 +99,36 @@
   position: absolute;
   top: 30%;
 }
-.contact-widget .contact-icon{display: inline-block;}
+.contact-widget .contact-icon{display: inline-block; margin-bottom: 10px;}
 .contact-widget .contact-icon i{
   font-size: 60px;
 }
+
+
 @media(max-width: 768px){
-  .contact-widget .contact-details{
-    top:0;
+  .contact-widget{
+    height: auto;
+  }
+  .contact-widget .bg-contact{
+    margin-left: -15px;
+    width: calc(100% + 30px);
+    height: 450px;
+    position: relative;
+  }
+  .contact-widget .overlay{ display: none; }
+  .contact-widget .container{ padding-top: 0px; }
+  .contact-widget h1{
+    color: #707070;
+    text-align: center;
+    margin-top: 40px;
+    margin-bottom: 40px;
+    font-size: 51px;
+  }
+  .contact-widget .contact-info{
+    display: block;
+  }
+  .contact-widget .contact-info h2{
+    font-size: 28px;
   }
 }
 </style>
