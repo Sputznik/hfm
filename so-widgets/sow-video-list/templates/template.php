7fbf9928384ac78445f88d2c145ecddf0f05ef63
Steve <anil.stephen66@gmail.com>
2020-01-08 16:39:16 +0530

    Added category attribute to sow-video-list

--- so-widgets/sow-video-list/templates/template.php
+++ so-widgets/sow-video-list/templates/template.php
@@ -1,11 +1,16 @@
 <?php
 
 $list_type = $instance['video_list_type'];
-$video_category = $instance['video_category'];
+$video_category = isset( $instance['video_category'] ) && $instance['video_category'] ? $instance['video_category'] : "";
 $count = $instance['video_count'];
 
 // Includes orbit template for different list layouts
-$shortcode_str = "[orbit_query post_type='videos' style='$list_type' posts_per_page='$count' tax_query='video_category:$video_category']";
+$shortcode_str = "[orbit_query post_type='videos' style='$list_type' posts_per_page='$count'";
+
+if( $video_category ){
+  $shortcode_str .= " tax_query='video_category:$video_category'";
+}
+$shortcode_str .= "]";
 
 echo do_shortcode( $shortcode_str );
 
