cf185675b03fb9e41700712a01bdafef4d1e21e1
Steve <anil.stephen66@gmail.com>
2020-02-07 12:15:06 +0530

    Updated blog-list-sow for posts

--- orbit-query/articles-list.php
+++ orbit-query/articles-list.php
@@ -16,6 +16,18 @@
     <?php endif;?>
     <div class='orbit-post-desc'>
       <h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
+      <?php $post_type = get_post_type(); if( $post_type == 'post' ):?>
+        <div class='post-author-info'>
+          <?php global $post;?>
+          <div class="post-author-avatar">
+            <img src="<?php _e( get_avatar_url( $post->post_author,array("size"=>60 ) ) );?>" alt="author-avatar">
+          </div>
+          <div class="post-author-meta">
+            <div class="post-author-name"><?php the_author();?></div>
+            <div class="post-author-date"><?php echo get_the_date('M j, Y');?></div>
+          </div>
+        </div>
+      <?php endif;?>
       <div class="orbit-post-excerpt"><?php echo excerpt(30);?></div>
       <?php echo get_template_part( "partials/content", "fields" );?>
     </div>
